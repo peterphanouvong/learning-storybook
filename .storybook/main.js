2dbc2a7c8e7bbfef5dc50d458b60fac7672a6941
Peter Phanouvong <peterphanouvong@gmail.com>
2022-01-02 11:03:03 +1100

    Task component + snapshot testing

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.jsx"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
---
+++ src/components/Task.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./task.css";
+
+export const Task = ({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}) => {
+  return (
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === "TASK_ARCHIVED"}
+          disabled={true}
+          name="checked"
+        />
+        <span
+          className="checkbox-custom"
+          onClick={() => onArchiveTask(id)}
+          id={`archiveTask-${id}`}
+          aria-label={`archiveTask-${id}`}
+        ></span>
+      </label>
+      <div className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+        />
+      </div>
+
+      <div className="actions" onClick={(event) => event.stopPropagation()}>
+        {state !== "TASK_ARCHIVED" && (
+          <a onClick={() => onPinTask(id)}>
+            <span
+              className="icon-star"
+              id={`pinTask-${id}`}
+              aria-label={`pinTask-${id}`}
+            ></span>
+          </a>
+        )}
+      </div>
+    </div>
+  );
+};
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func,
+};
